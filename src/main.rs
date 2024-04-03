d3c9e2e2d7d9da792f5b3c1a23284ce38f4b1aae
EasyPeasyViking <easypeasyviking@gmail.com>
2024-04-03 21:50:38 +0200

    add a first greeting example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", greet());
+}
+
+fn greet() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::greet;
+
+    #[test]
+    fn it_returns_a_greeting_message() {
+        let result = greet();
+        assert_eq!(result, "Hello, world!");
+    }
 }
